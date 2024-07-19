0669e022f6a9261da30d6ddd983c8288fb88f7db
Nero <paul.bogza@gmail.com>
2024-07-19 13:41:10 +0200

    parsing done hopefully

--- src/parser.cpp
+++ src/parser.cpp
@@ -8,35 +8,36 @@
 void parseAndAdd(int counter, std::string line, std::map<std::string, std::vector<Node>> *graph){
     std::vector<Node> tempVector;
     Node tempNode;
-    std::string lineDelimiter = ":";
+    std::string lineDelimiter = ": ";
     std::string stationDelimiter = "\"";
     std::string weightDelimiter = " ";
-    std::string lineName = NULL;
-    std::string stationName = NULL;
-    std::string tempWeight = NULL;
+    std::string lineName = "NULL";
+    std::string stationName = "NULL";
+    std::string tempWeight = "NULL";
     int weight = 0;
 
     //split string for train number e.g U1, U2 etc. and then erase it
     lineName = line.substr(0, line.find(lineDelimiter));
-    line.erase(0, line.find(weightDelimiter) + weightDelimiter.length()+1);
+    line.erase(0, line.find(lineDelimiter) + lineDelimiter.length()+1);
 
     while(!line.empty()){
         //split string for station name e.g Leopoldau etc. and then erase it
-        stationName = line.substr(2, line.find(stationDelimiter));
-        //stationName = stationName.substr(1, stationName.size()-2);
-        line.erase(0, line.find(weightDelimiter) + weightDelimiter.length()+1);
+        stationName = line.substr(0, line.find(stationDelimiter));
+        line.erase(0, line.find(stationDelimiter) + stationDelimiter.length()+1);
+        tempNode.name = stationName; 
+        std::cout << line << std::endl;
 
-        //split string for edge weight and then erase it
-        tempWeight = line.substr(0, line.find(weightDelimiter));
-        weight = std::stoi(tempWeight, &sz);
-        line.erase(0, line.find(weightDelimiter) + weightDelimiter.length()+1);
+        if(!line.empty()){
+            //split string for edge weight and then erase it
+            tempWeight = line.substr(0, line.find(weightDelimiter));
+            weight = std::stoi(tempWeight);
+            line.erase(0, line.find(weightDelimiter) + weightDelimiter.length()+1);
+            tempNode.weight = weight;
+        }
 
-        //tempNode.name = stationName;
-        //tempNode.weight = weight;
-
-        //tempVector.push_back(tempNode);
+        tempVector.push_back(tempNode);
     }
-    //graph->insert({lineName, tempVector});
+    graph->insert({lineName, tempVector});
 }
 
 void createGraph(std::map<std::string, std::vector<Node>> *graph, std::string stops){
