ad100ceb92917dad9c7cda92a0601bcfbaeccb37
PaulFHTW <paul.bogza@gmail.com>
2025-04-26 17:06:01 +0200

    errors

--- src/dijkstra.cpp
+++ src/dijkstra.cpp
@@ -15,6 +15,8 @@ std::tuple<std::vector<Node*>, int> findPath(const std::vector<Node*> &graph, co
     int tempDistance = 0;
     int tempWeight = 999;
 
+    std::cout << dest->name << std::endl;
+
     if(!unvisitedNodes.empty()){
         for(auto i : unvisitedNodes){
             if(i->name == start->name){
@@ -29,10 +31,10 @@ std::tuple<std::vector<Node*>, int> findPath(const std::vector<Node*> &graph, co
         do{
             if(currentNode.neighbours.size() > 0){
                 for(auto i : currentNode.neighbours){
-                    //std::cout << i->station->name << std::endl;
-                    if((i->station->visited == false) && (i->weight < tempWeight)){
+                    if((i->station != nullptr) && (i->station->visited == false) && (i->weight < tempWeight)){
                         tempWeight = i->weight;    
                         tempNode = *i->station;
+                        std::cout << tempNode.name << std::endl;
                     }
                 }
                 tempPath.emplace_back(&currentNode);
@@ -45,6 +47,10 @@ std::tuple<std::vector<Node*>, int> findPath(const std::vector<Node*> &graph, co
         
         path = tempPath;
         distance = tempDistance;
+
+        //for(auto i : path){
+        //    std::cout << i->name << std::endl;
+        //}
 
         return std::make_pair(path, distance);
     }
