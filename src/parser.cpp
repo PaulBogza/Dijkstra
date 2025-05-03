aa74e98296caf1d7122061c783aa0ca848bb6a67
PaulFHTW <paul.bogza@gmail.com>
2025-05-03 14:17:46 +0200

    fuck me

--- src/parser.cpp
+++ src/parser.cpp
@@ -96,12 +96,10 @@ void createGraph(std::vector<Node*> &graph, std::string stops){
         for(int i = 0; i < graph.size(); i++){
             for(int j = i+1; j < graph.size(); j++){
                 for(auto k : graph.at(j)->neighbours){
-					if(k->station != nullptr){
-						if(graph.at(i)->name == k->station->name){
-							k->station = graph.at(i);
-							std::cout << &graph.at(i) << " " << graph.at(i)->name << std::endl;
-							std::cout << &k->station << " " << k->station->name << std::endl;
-						}
+					if(k->station != nullptr && graph.at(i)->name == k->station->name){
+						k->station = graph.at(i);
+						std::cout << &graph.at(i) << " " << graph.at(i)->name << std::endl;
+						std::cout << &k->station << " " << k->station->name << std::endl;
 					}
                 }
             }
